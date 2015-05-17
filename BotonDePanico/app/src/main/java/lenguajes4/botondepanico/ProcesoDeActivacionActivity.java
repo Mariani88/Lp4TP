154c70ba3b765263fd3e7ee8163ecea62609fd8f
Mariani88 <pm_mariani@hotmail.com>
2015-05-16 22:02:50 -0300

    subo primer version proyecto

---
+++ BotonDePanico/app/src/main/java/lenguajes4/botondepanico/ProcesoDeActivacionActivity.java
@@ -0,0 +1,65 @@
+package lenguajes4.botondepanico;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ProcesoDeActivacionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proceso_de_activacion);
+
+        Button botonConfirmar = (Button)findViewById(R.id.botonConfirmarAlarma);
+
+        botonConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProcesoDeActivacionActivity.this
+                        ,EnvioDeSMSActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button cancelarAlarma = (Button)findViewById(R.id.botonCancelarAlarma);
+
+        cancelarAlarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (ProcesoDeActivacionActivity.this,
+                        CancelarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_proceso_de_activacion, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
